2a952de47f71ab88a7058651acadf9f433fecd2d
Damjanac Marko <36675797+damlern92@users.noreply.github.com>
2021-06-20 16:09:21 +0200

    Improved comments and code structure

--- server/index.php
+++ server/index.php
@@ -1,6 +1,3 @@
-
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -16,14 +13,15 @@
 </head>
 <body>
 
-
 <div class="container">
     <div class="row">
         <div class="col-md-6 col-md-offset-3">
             <div class="panel panel-login">
+                <!-- .panel-heading: -->
                 <div class="panel-heading">
                     <div class="row">
                         <div class="col-xs-6">
+
 
                             <div id="success" style="display:none;"></div>
 
@@ -38,21 +36,26 @@
                     </div>
                     <hr>
                 </div>
+                <!-- .panel-body: -->
                 <div class="panel-body">
                     <div class="row">
                         <div class="col-lg-12">
 
                             <form id="login-form" action="#" method="post" role="form" style="display: block;">
+
                                 <div class="form-group">
                                     <input type="text" name="username" id="username" tabindex="1" class="form-control" placeholder="Username" value="">
                                 </div>
+
                                 <div class="form-group">
                                     <input type="password" name="password" id="password" tabindex="2" class="form-control" placeholder="Password">
                                 </div>
+
                                 <div class="form-group text-center">
                                     <input type="checkbox" tabindex="3" class="" name="remember" id="remember">
                                     <label for="remember"> Remember Me</label>
                                 </div>
+
                                 <div class="form-group">
                                     <div class="row">
                                         <div class="col-sm-6 col-sm-offset-3">
@@ -60,6 +63,7 @@
                                         </div>
                                     </div>
                                 </div>
+
                                 <div class="form-group">
                                     <div class="row">
                                         <div class="col-lg-12">
@@ -69,9 +73,11 @@
                                         </div>
                                     </div>
                                 </div>
-                            </form>
+
+                            </form> <!-- end of #login-form -->
 
                             <?php
+                            // For login submit:
                                 session_start();
 
                                 if(isset($_POST['login-submit'])){
@@ -85,15 +91,9 @@
                             ?>
 
 
-
-
-
-
                             <!-- Show the message from the success object: -->
                             <div id="success" style="display: none;"></div>
                             
-
-
                             
                             <form id="register-form" action="#" method="post" role="form" style="display: none;">
                                 <div class="form-group">
@@ -116,16 +116,18 @@
                                         </div>
                                     </div>
                                 </div>
-                            </form>
+                            </form> <!-- end of #register-form -->
 
 
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+                        </div><!-- end of.col-lg-12 -->
+                    </div><!-- end of .row -->
+                </div><!-- end of .panel-body -->
+
+            </div><!-- end of .panel panel-login -->
+        </div><!-- end of .col-md-6 col-md-offset-3 -->
+
+    </div><!-- end of first .row -->
+</div><!-- end of .container -->
 
 
 
